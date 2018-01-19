2649fade974e3f4bc84273bd6cddaf749d0387d5
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2018-01-19 21:44:01 +0100

    Test for include_role/honour_allow_duplicates

--- test/integration/targets/include_import/runme.sh
+++ test/integration/targets/include_import/runme.sh
@@ -94,3 +94,8 @@ test "$(egrep -c 'include handler task|ERROR! The requested handler '"'"'do_impo
 # https://github.com/ansible/ansible/issues/49969
 ansible-playbook -v parent_templating/playbook.yml 2>&1 | tee test_parent_templating.out
 test "$(egrep -c 'Templating the path of the parent include_tasks failed.' test_parent_templating.out)" = 0
+
+## Include role honour allow duplicates
+# https://github.com/ansible/ansible/pull/35164
+ANSIBLE_STRATEGY='linear' ansible-playbook playbook/honour_duplicates.yml -i inventory "$@"
+ANSIBLE_STRATEGY='free' ansible-playbook playbook/honour_duplicates.yml -i inventory "$@"
