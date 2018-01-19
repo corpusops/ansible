cc2ed7fee0dfa20ac6e8094d73437b2bb90b20bc
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2018-01-19 21:44:01 +0100

    Test for include_role/honour_allow_duplicates

---
+++ test/integration/targets/include_role_honour_duplicates/runme.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+W=$(cd "$(dirname "$0")" && pwd)
+export ANSIBLE_ROLES_PATH="$W/roles"
+set -eux
+ansible-playbook -i../../inventory test.yml -v
+ansible-playbook -i../../inventory test2.yml -v
